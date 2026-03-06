9f711a0b2f7dd529ee9ff56b7305f3bee71d744b
Ms-Schnitzel <zachnielsen72@gmail.com>
2026-03-05 20:09:14 -0700

    dynamic arrays assignment

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +15,20 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // First, I need an array to hold the multiples that I am going to return, I'll name it 'multiples'
+        // Next, I need to iterate through the variable 'number' for the amount of times that is given in 'length', so I will use a for loop where 'i' is checked against length to know when to stop
+        // While iterating through the multiples, the index of 'multiples' will be assigned based on the current value of 'i' and will be calculated by taking the input number and multiplying it by the number of iterations we are on
+        // Since 'i' starts at 0, it will be measured as i + 1
+        // return multiples
+
+        var multiples = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = (number * (i + 1));
+        }
+
+        return multiples;
     }
 
     /// <summary>
@@ -29,5 +44,21 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // For a list to be rotated to the right, the last number of the list would be moved to the front.
+        // if we use data.Count we can find the length of the list and get the index of the last value as data.Count - 1
+        // Then, we store the value and use data.RemoveAt(data.Count - 1) to pluck it from the array
+        // Once it's removed, we then re-add it at the beginning using data.Insert(0, value)
+        // All of that is then enclosed in a for loop where i is less than the provided amount to continue to rotate the data
+
+        int len = data.Count;
+        for (int i = 0; i < amount; i++)
+        {
+            int val = data[len - 1];
+            data.RemoveAt(len - 1);
+            data.Insert(0, val);
+            Debug.WriteLine(data);
+        }
+        Debug.WriteLine(data);
     }
 }
